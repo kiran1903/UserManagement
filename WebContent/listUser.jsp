4367437f3057fb3521f68761097368b9c6c3b30f
kiran <20100681@mail.wit.ie>
2023-06-30 01:40:50 +0100

    Initial Commit

---
+++ WebContent/listUser.jsp
@@ -0,0 +1,50 @@
+<%@include file="include/header.jsp"%>
+<%@ page import="java.util.List"%>
+<%@ page import="org.studyeasy.entity.User"%>
+
+<div class="container mtb">
+	<div class="row">
+		<div class="col-lg-6">
+			<strong>Listing users</strong>
+			<hr />
+			<table border="1">
+				<thead>
+					<th>User ID</th>
+					<th>Username</th>
+					<th>Email</th>
+					<th>Operation</th>
+				</thead>
+				<%!String deleteURL;%>
+				<%
+					List<User> listUsers = (List) request.getAttribute("listUsers");
+					String updateURL;
+					for (int i = 0; i < listUsers.size(); i++) {
+						out.print("<tr>");
+						out.print("<td>" + listUsers.get(i).getUsers_id() + "</td>");
+						out.print("<td>" + listUsers.get(i).getUsername() + "</td>");
+						out.print("<td>" + listUsers.get(i).getEmail() + "</td>");
+						updateURL = request.getContextPath() + "/operation?page=updateUser" + "&usersId="
+								+ listUsers.get(i).getUsers_id() + "&username=" + listUsers.get(i).getUsername() + "&email="
+								+ listUsers.get(i).getEmail();
+						deleteURL = request.getContextPath() + "/operation?page=deleteUser" + "&usersId="
+								+ listUsers.get(i).getUsers_id();
+						out.print("<td><a href=" + updateURL + ">Update</a>|");
+				%>
+				<a href="<%=deleteURL%>"
+					onclick="if(!confirm('Are you sure to delete the user?')) return false">Delete</a>
+				</td>
+				</tr>
+				<%
+					}
+				%>
+			</table>
+		</div>
+	</div>
+</div>
+
+<%@include file="include/footer.jsp"%>
+
+
+
+
+
---
+++ src/org/studyeasy/controller/OperationController.java
@@ -0,0 +1,120 @@
+package org.studyeasy.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import org.studyeasy.entity.User;
+import org.studyeasy.model.UsersModel;
+
+@WebServlet("/operation")
+public class OperationController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	@Resource(name = "jdbc/project")
+	private DataSource dataSource;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String page = request.getParameter("page");
+		page = page.toLowerCase();
+
+		switch (page) {
+		case "listusers":
+			listUsers(request, response);
+			break;
+		case "adduser":
+			addUserFormLoader(request, response);
+			break;
+		case "updateuser":
+			UpdateUserFormLoader(request, response);
+			break;
+		case "deleteuser":	
+		     deleteUser(Integer.parseInt(request.getParameter("usersId")));
+		     listUsers(request, response);
+		     break;
+		default:
+			errorPage(request, response);
+		}
+
+	}
+
+	private void deleteUser(int usersID) {
+		new UsersModel().deleteUser(dataSource,usersID);
+		return;
+	}
+
+	private void UpdateUserFormLoader(HttpServletRequest request, HttpServletResponse response) {
+		request.setAttribute("title", "Update User");
+		try {
+			request.getRequestDispatcher("updateUser.jsp").forward(request, response);
+		} catch (ServletException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String operation = request.getParameter("form");
+		operation = operation.toLowerCase();
+		switch (operation) {
+		case "adduseroperation":
+			User newUser = new User(request.getParameter("username"), request.getParameter("email"));
+			addUserOperation(newUser);
+			listUsers(request, response);
+			break;
+		case "updateuseroperation":
+			User updatedUser = new User(Integer.parseInt(request.getParameter("usersId")),
+					request.getParameter("username"), request.getParameter("email"));
+			updateUserOperation(dataSource, updatedUser);
+			listUsers(request, response);
+			break;
+		default:
+			errorPage(request, response);
+			break;
+		}
+	}
+
+	private void updateUserOperation(DataSource dataSource, User updatedUser) {
+		new UsersModel().updateUser(dataSource,updatedUser);
+		return;
+		
+	}
+
+	private void addUserOperation(User newUser) {
+		new UsersModel().addUser(dataSource, newUser);
+		return;
+	}
+
+	public void listUsers(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		List<User> listUsers = new ArrayList<>();
+		listUsers = new UsersModel().listUsers(dataSource);
+		request.setAttribute("listUsers", listUsers);
+		request.setAttribute("title", "List of users");
+		request.getRequestDispatcher("listUser.jsp").forward(request, response);
+
+	}
+
+	public void addUserFormLoader(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setAttribute("title", "Add User");
+		request.getRequestDispatcher("addUser.jsp").forward(request, response);
+
+	}
+
+	public void errorPage(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setAttribute("title", "Error page");
+		request.getRequestDispatcher("error.jsp").forward(request, response);
+
+	}
+}
